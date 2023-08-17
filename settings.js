873a7b88188b1a186befc4ce5a89df96b62a2123
Riissss <121730144+Riissss@users.noreply.github.com>
2023-08-18 05:27:33 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 global.creator = "ohmyriiss28";
-global.apikey = ["omr28", "free", "ohmyriiss28"];
+global.apikey = ["Elistz", "Mwuhehe", "Rainchy"];
 
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
